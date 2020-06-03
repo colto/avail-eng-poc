d429d4b5a01e86f70374043dd8fb78152b855ab5
colto <coltomatic@gmail.com>
2020-06-03 14:52:16 -0700

    Adding validation

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,7 +1,18 @@
 # frozen_string_literal: true
 
 class Asset < ApplicationRecord
+  validates :active_at, presence: true
+  validate :active_and_inactive_overlap
+
   def self.for_time_period(start_at, end_at)
     where('active_at <= ? AND (inactive_at is NULL or inactive_at >= ?)', start_at, end_at)
+  end
+
+  private
+
+  def active_and_inactive_overlap
+    return if inactive_at.blank?
+
+    errors.add(:inactive_at, 'must be after the active date') if inactive_at < active_at
   end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,10 +1,19 @@
 # frozen_string_literal: true
 
 class Booking < ApplicationRecord
+  validates :start_at, presence: true
+  validates :end_at, presence: true
+  validate :start_and_end_overlap
+
   scope :by_length, -> { order(Arel.sql('extract(EPOCH from end_at - start_at) DESC')) }
   scope :for_time_period, ->(start_at, end_at) { where('start_at <= ? and end_at >= ?', end_at, start_at) }
 
   def overlap?(booking)
     !(start_at >= booking.end_at || end_at <= booking.start_at)
+  end
+
+  private
+  def start_and_end_overlap
+    errors.add(:end_at, 'must be after the start date') if end_at < start_at
   end
 end
